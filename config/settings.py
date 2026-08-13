6fe75047ee60c62691abc1807c1f2e2d8eada8e4
cgmonali <cgmonali@gmail.com>
2026-08-14 02:45:01 +0530

    feat: configure PostgreSQL, DRF, and JWT authentication

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "rest_framework",
+    "users",
+    "rooms",
+    "chat",
 ]
 
 MIDDLEWARE = [
@@ -73,9 +78,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "traderoom",
+        "USER": "traderoom",
+        "PASSWORD": "traderoom_password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -124,4 +133,12 @@ MAILERS = {
     'default': {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
+}
+
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
